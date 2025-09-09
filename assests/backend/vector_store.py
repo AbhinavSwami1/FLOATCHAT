f35b4ec7bb869df219d067d249a45f9df2f49a01
manviig <manvi6262@gmail.com>
2025-09-10 01:53:12 +0530

    vector files added

--- assests/backend/vector_store.py
+++ assests/backend/vector_store.py
@@ -1,17 +1,17 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings  # ya HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-# Metadata read karo
-with open("../dataset/ar_index_global_meta.txt", "r") as f:
+# Metadata read
+with open("../dataset/ar_index_global_meta.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 # Split into chunks
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = splitter.split_text(text)
 
-# Embeddings banao
-embeddings = OpenAIEmbeddings()   # HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") bhi use kar sakta hai
+# HuggingFace embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # Store in FAISS
 db = FAISS.from_texts(docs, embeddings)
